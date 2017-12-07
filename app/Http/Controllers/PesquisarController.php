7e1513781dd8266edc7096513523c728d25dedfa
jutente <juliano.tda@gmail.com>
2017-12-07 13:30:07 -0200

    pesquisar controller

---
+++ app/Http/Controllers/PesquisarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+
+class PesquisarController extends Controller
+{
+    function verificar (){
+
+      //  dd($request());
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 route::get('/matricula', function(){
     return view('matricula');
 });
+
+//route::post('/matricula', 'PesquisarController');
